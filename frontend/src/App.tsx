d220a91904446a5e15193f25a7609204ff65420e
HB4305 <hoaichaobai@gmail.com>
2025-11-30 23:43:36 +0700

    SOIC-8: Change App.css, Changes all page use Router, Use English cmt

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import LoginForm from './pages/Login'
 import ResetPassword from "./pages/ResetPassword";
+import Register from "./pages/Register";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<LoginForm />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
         <Route path='/reset-password' element={<ResetPassword />} />
       </Routes>
     </BrowserRouter>
