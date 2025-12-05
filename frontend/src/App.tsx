ec9f91a9092f8158247b577bfa2766f5904d6925
Loctran0405 <thloc23@clc.fitus.edu.vn>
2025-12-05 10:43:36 +0700

    SOIC-21: FIX UI optimistic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,18 +7,37 @@ import ForgotPassword from "./pages/ForgotPassword";
 import Feed from "./pages/Feed";
 // import CreatePostDialog from './components/CreatePostDialog';
 
+import { useState } from 'react';
+import CreatePostDialog from '@/components/CreatePostDialog'; 
+import { CURRENT_USER, MOCK_FRIENDS } from "@/MockData/data";
 
 export default function App() {
+  const [isDialogOpen, setIsDialogOpen] = useState(true);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/login' element={<LoginForm />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/forgot-password' element={<ForgotPassword />} />
-        <Route path='/reset-password' element={<ResetPassword />} />
-        <Route path='/' element={<Feed />} />
-      </Routes>
-    </BrowserRouter>
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<LoginForm />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/reset-password' element={<ResetPassword />} />
+          <Route path='/' element={<Feed />} />
+        </Routes>
+      </BrowserRouter>
+
+      <CreatePostDialog 
+        open={isDialogOpen} 
+        onOpenChange={setIsDialogOpen} 
+        currentUser={CURRENT_USER} 
+        mockFriends={MOCK_FRIENDS} 
+        onPost={(content, mediaFiles) => {
+          console.log("Đã gửi bài đăng với nội dung:", content, "và media:", mediaFiles);
+          setIsDialogOpen(false); 
+        }}
+      />
+    </>
+  );
 }
+
 
