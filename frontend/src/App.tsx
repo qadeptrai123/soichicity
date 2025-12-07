86c79aa3507b971ddb445eb2c2afe76ec762d65c
thekeivn <201560484+thekeivn16@users.noreply.github.com>
2025-12-07 09:16:13 +0700

    SOIC-31

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,58 +1,3 @@
-// import { BrowserRouter, Routes, Route } from 'react-router-dom'
-// import './App.css'
-// import LoginForm from './pages/Login'
-// import ResetPassword from "./pages/ResetPassword";
-// import Register from "./pages/Register";
-// import ForgotPassword from "./pages/ForgotPassword";
-// import Feed from "./pages/Feed";
-// // import CreatePostDialog from './components/CreatePostDialog';
-
-
-// export default function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path='/login' element={<LoginForm />} />
-//         <Route path='/register' element={<Register />} />
-//         <Route path='/forgot-password' element={<ForgotPassword />} />
-//         <Route path='/reset-password' element={<ResetPassword />} />
-//         <Route path='/' element={<Feed />} />
-//       </Routes>
-//     </BrowserRouter>
-//   )
-// }
-
-// import { BrowserRouter, Routes, Route } from 'react-router-dom'
-// import './App.css'
-
-// import LoginForm from './pages/Login'
-// import ResetPassword from "./pages/ResetPassword"
-// import Register from "./pages/Register"
-// import ForgotPassword from "./pages/ForgotPassword"
-// import Feed from "./pages/Feed"
-
-// import Layout from './components/layout/Layout'
-// export default function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         {/* QUAN TRỌNG: Route Layout bao bọc các Route con */}
-//         <Route element={<Layout />}>
-          
-//           {/* path="/" chính là trang Feed sẽ hiện vào vị trí <Outlet /> */}
-//           <Route path="/" element={<Feed />} />
-          
-//           {/* Các trang khác */}
-//           <Route path="/search" element={<div className="text-white">Search</div>} />
-//         </Route>
-
-//         {/* Các route không có layout (Login/Register) để ra ngoài */}
-//         <Route path="/login" element={<div>Login Page</div>} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 
@@ -61,32 +6,38 @@ import ResetPassword from "./pages/ResetPassword"
 import Register from "./pages/Register"
 import ForgotPassword from "./pages/ForgotPassword"
 import Feed from "./pages/Feed"
+// 1. Import trang chi tiết (Bạn cần đảm bảo đã tạo file này ở bước trước)
+import ThreadDetail from "./pages/ThreadDetail" 
 
 import Layout from './components/layout/Layout'
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Routes>
+      <Routes>
 
-      {/* AUTH ROUTES */}
-      <Route path="/login" element={<LoginForm />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="/reset-password" element={<ResetPassword />} />
+        {/* AUTH ROUTES */}
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
 
-      {/* APP ROUTES */}
-      <Route element={<Layout />}>
-        <Route index element={<Feed />} />
+        {/* APP ROUTES */}
+        {/* Layout bọc các trang bên trong để hiển thị Sidebar/Topbar chung */}
+        <Route element={<Layout />}>
+          <Route index element={<Feed />} />
 
-        <Route path="search" element={<div className="text-white">Search</div>} />
-        <Route path="favorites" element={<div className="text-white">Favorites</div>} />
-        <Route path="community" element={<div className="text-white">Community</div>} />
-        <Route path="create" element={<div className="text-white">Create</div>} />
-      </Route>
+          {/* 2. Thêm Route chi tiết bài viết */}
+          {/* :id là tham số động, ví dụ: /thread/123, /thread/abc */}
+          <Route path="thread/:id" element={<ThreadDetail />} />
 
-    </Routes>
-  </BrowserRouter>
+          <Route path="search" element={<div className="text-white">Search</div>} />
+          <Route path="favorites" element={<div className="text-white">Favorites</div>} />
+          <Route path="community" element={<div className="text-white">Community</div>} />
+          <Route path="create" element={<div className="text-white">Create</div>} />
+        </Route>
 
+      </Routes>
+    </BrowserRouter>
   );
 }
