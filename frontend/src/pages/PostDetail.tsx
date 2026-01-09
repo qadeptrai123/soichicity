c6a16e1c87d3d6452c63b3e302e73d6ff14473de
HB4305 <hoaichaobai@gmail.com>
2026-01-09 20:07:21 +0700

    Remove scroll-bar in view detail post

--- frontend/src/pages/PostDetail.tsx
+++ frontend/src/pages/PostDetail.tsx
@@ -94,7 +94,7 @@ const PostDetail = () => {
                 <div className={`w-[700px] h-full ${COLORS.bgCard} rounded-[32px] border ${COLORS.border} shadow-2xl flex flex-col z-10 relative overflow-hidden`}>
 
                     {/* Scrollable Area */}
-                    <div className="flex-1 overflow-y-auto custom-scrollbar p-0">
+                    <div className="flex-1 overflow-y-auto no-scrollbar p-0">
 
                         {/* Bài Post */}
                         <PostMainPost
